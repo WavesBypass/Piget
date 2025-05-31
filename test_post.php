7f605ce3dc511531e8aa02fc1d8438e9241675dc
Hjr <john789811@gmail.com>
2025-05-31 18:30:56 +1000

    Delete test_post.php

--- test_post.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "POST received. Username: " . htmlspecialchars($_POST['username']);
-} else {
-    echo "Send a POST request to this page.";
-}
-?>
